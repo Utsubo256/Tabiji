25a51bbeb034b6f64af184c192becebab33879fd
Utsubo256 <greeeentea256@gmail.com>
2022-11-27 19:57:11 +0900

    name属性にバリデーションを追加し、存在性を検証するテストを追加

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe User, type: :model do
     )
     expect(user).to be_valid
   end
+
+  it 'is invalid without a name' do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
 end
