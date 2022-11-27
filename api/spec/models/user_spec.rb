3d1eca86b9cf6d39785417d25800f159a21af5f6
Utsubo256 <greeeentea256@gmail.com>
2022-11-27 19:38:28 +0900

    Userモデルの有効性を検証するテストを追加

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name, email, and introduction' do
+    user = User.new(
+      name: 'Example User',
+      email: 'user@example.com',
+      introduction: 'Hello!',
+    )
+    expect(user).to be_valid
+  end
 end
