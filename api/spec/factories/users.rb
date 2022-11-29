b29fbc809279c73dfd5f9549c32e7be37912dca8
Utsubo256 <greeeentea256@gmail.com>
2022-11-29 15:57:04 +0900

    Userモデルにhas_secure_passwordを追加

--- api/spec/factories/users.rb
+++ api/spec/factories/users.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     name { "user_name" }
     email { "foobar@example.com" }
     introduction { "Hello!" }
+    password { "foobar" }
+    password_confirmation { "foobar" }
   end
 end
