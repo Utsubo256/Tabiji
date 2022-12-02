58f17b5aba888c127b5267f031720b887320145f
Utsubo256 <greeeentea256@gmail.com>
2022-12-02 14:31:42 +0900

    Usersコントローラのshowアクションに対するテストを追加

--- api/spec/requests/api/v1/users_spec.rb
+++ api/spec/requests/api/v1/users_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "API::V1::Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  it 'loads a user' do
+    user = FactoryBot.create(:user)
+
+    get api_v1_user_path(user)
+    expect(response).to have_http_status(:ok)
+
+    json = JSON.parse(response.body)
+    expect(json['name']).to eq user.name
+    expect(json['email']).to eq user.email
+    expect(json['introduction']).to eq user.introduction
   end
 end
