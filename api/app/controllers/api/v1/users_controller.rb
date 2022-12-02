528cad97cad5bcc812872a74a575d1d226823827
Utsubo256 <greeeentea256@gmail.com>
2022-12-02 15:20:04 +0900

    Usersコントローラのshowアクションを追加し、テスト通過を確認

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class API::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
+  def show
+    render json: {
+      name: @user.name,
+      email: @user.email,
+      introduction: @user.introduction
+    }, status: :ok
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
