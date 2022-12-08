fef5e1d5ac93a0a8be835db078a571cb0b176a63
Utsubo256 <greeeentea256@gmail.com>
2022-12-08 17:55:41 +0900

    React v17からimport React from reactが不要になったため、ESLintに引っかからないようにするための設定を追加

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -23,6 +23,7 @@ module.exports = {
     ],
     "rules": {
         "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
+        "react-hooks/exhaustive-deps": "warn",
+        "react/react-in-jsx-scope": "off"
     }
 }
