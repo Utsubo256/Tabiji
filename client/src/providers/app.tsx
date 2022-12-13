f3b891442c1c35330365b7d8ef24801dea3a7226
Utsubo256 <greeeentea256@gmail.com>
2022-12-13 17:06:20 +0900

    react-queryとreact-query/devtoolsが使えるようプロバイダを追加

--- client/src/providers/app.tsx
+++ client/src/providers/app.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import { queryClient } from '@/lib/react-query';
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -9,9 +13,12 @@ type AppProviderProps = {
 export function AppProvider({ children }: AppProviderProps) {
   return (
     <React.Suspense>
-      <ChakraProvider>
-        <Router>{children}</Router>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        {import.meta.env.NODE_ENV !== 'test' && <ReactQueryDevtools />}
+        <ChakraProvider>
+          <Router>{children}</Router>
+        </ChakraProvider>
+      </QueryClientProvider>
     </React.Suspense>
   );
 }
