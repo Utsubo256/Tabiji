9404fe27fbab03348c2b868506312818b40b6e75
Utsubo256 <greeeentea256@gmail.com>
2022-12-15 09:48:52 +0900

    ユーザー登録ページのひな形を作成し、/signupへのパスを追加

--- client/src/routes/public.tsx
+++ client/src/routes/public.tsx
@@ -1,5 +1,7 @@
+import { Register } from '@/features/auth/routes/Register';
 import { Profile } from '@/features/users/routes/Profile';
 
 export const publicRoutes = [
+  { path: '/signup', element: <Register />},
   { path: '/users/:userId', element: <Profile />}
 ];
