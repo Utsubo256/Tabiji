31a29c3b7551ed0bc3fe86d7ff6aa8c1a2d70636
Utsubo256 <greeeentea256@gmail.com>
2023-01-19 20:37:51 +0900

    スネークケースからキャメルケースへ自動で変換するよう実装

--- client/src/lib/apiClient.ts
+++ client/src/lib/apiClient.ts
@@ -1,11 +1,17 @@
+import applyCaseMiddleware from 'axios-case-converter';
 import Axios from 'axios';
 
 import { API_URL } from '@/config';
 
-export const ApiClient = Axios.create({
+const options = {
+  ignoreHeaders: true
+}
+
+export const ApiClient = applyCaseMiddleware(Axios.create({
   baseURL: API_URL,
-});
+}), options);
 
 ApiClient.interceptors.response.use(
   (response) => response.data,
 )
+
